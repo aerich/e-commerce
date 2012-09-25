9a2b62a77a7d8253bc00447ee3c7f471463475d4
Guillaume Pasbecq <Guy@Peltier.localdomain>
2012-09-25 09:56:41 +0200

    premier jet

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        
+        <ul>
+            <li class="bj1"></li>
+            <li class="bj2"></li>
+            <li class="bj3"></li>
+        </ul>
+        
+    <?php
+        // put your code here
+        phpinfo();
+        ?>
+    </body>
+</html>
