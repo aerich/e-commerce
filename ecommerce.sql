9eca133d3f5fd2167eca6935d05245b9f21dbd40
Guillaume Pasbecq <pgvcr@hotmail.com>
2012-11-21 11:51:03 +0100

    Mise à jour DB

--- ecommerce.sql
+++ ecommerce.sql
@@ -1,73 +1,96 @@
--- phpMyAdmin SQL Dump
--- version 3.4.5
--- http://www.phpmyadmin.net
---
--- Client: localhost
--- Généré le : Mar 02 Octobre 2012 à 09:49
--- Version du serveur: 5.5.16
--- Version de PHP: 5.3.8
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr" dir="ltr">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link rel="icon" href="./favicon.ico" type="image/x-icon" />
+    <link rel="shortcut icon" href="./favicon.ico" type="image/x-icon" />
+    <title>phpMyAdmin</title>
+    <link rel="stylesheet" type="text/css" href="phpmyadmin.css.php?server=1&amp;token=ad8b5db0c68a8b20fa3ec7622563e0cd&amp;js_frame=right&amp;nocache=5301623208" />
+    <link rel="stylesheet" type="text/css" href="print.css" media="print" />
+    <link rel="stylesheet" type="text/css" href="./themes/pmahomme/jquery/jquery-ui-1.8.custom.css" />
+    <meta name="robots" content="noindex,nofollow" />
+<script src="./js/cross_framing_protection.js?ts=1316000894" type="text/javascript"></script>
+<script src="./js/jquery/jquery-1.4.4.js?ts=1316000894" type="text/javascript"></script>
+<script src="./js/update-location.js?ts=1316000894" type="text/javascript"></script>
+<script src="./js/functions.js?ts=1316000894" type="text/javascript"></script>
+<script src="./js/jquery/jquery.qtip-1.0.0.min.js?ts=1316000894" type="text/javascript"></script>
+<script src="./js/messages.php?lang=fr&amp;db=ecommerce&amp;token=ad8b5db0c68a8b20fa3ec7622563e0cd" type="text/javascript"></script>
+<script type="text/javascript">
+// <![CDATA[
+// Updates the title of the frameset if possible (ns4 does not allow this)
+if (typeof(parent.document) != 'undefined' && typeof(parent.document) != 'unknown'
+    && typeof(parent.document.title) == 'string') {
+    parent.document.title = 'localhost / localhost / ecommerce | phpMyAdmin 3.4.5';
+}
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+// ]]>
+</script>
+        <meta name="OBGZip" content="true" />
+                <!--[if IE 6]>
+        <style type="text/css">
+        /* <![CDATA[ */
+        html {
+            overflow-y: scroll;
+        }
+        /* ]]> */
+        </style>
+        <![endif]-->
+    </head>
 
+    <body>
+        <div id="serverinfo">
+<a href="main.php?token=ad8b5db0c68a8b20fa3ec7622563e0cd" class="item">        <img class="icon" src="./themes/pmahomme/img/s_host.png" width="16" height="16" alt="" /> 
+localhost</a>
+        <span class="separator"><img class="icon" src="./themes/pmahomme/img/item_ltr.png" width="5" height="9" alt="-" /></span>
+<a href="db_structure.php?db=ecommerce&amp;token=ad8b5db0c68a8b20fa3ec7622563e0cd" class="item">        <img class="icon" src="./themes/pmahomme/img/s_db.png" width="16" height="16" alt="" /> 
+ecommerce</a>
+</div>
+<!-- PMA-SQL-ERROR -->
+    <div class="error"><h1>Erreur</h1>
+    <p><strong>Requête SQL:</strong>
+<a href="db_sql.php??sql_query=SET+time_zone+%3D+%22%2B00%3A00%22&amp;show_query=1&amp;db=ecommerce&amp;token=ad8b5db0c68a8b20fa3ec7622563e0cd"><span class="nowrap"><img src="./themes/pmahomme/img/b_edit.png" title="Modifier" alt="Modifier" class="icon" width="16" height="16" /> Modifier</span></a>    </p>
+    <p>
+        <span class="syntax"><span class="inner_sql"><a href="./url.php?url=http%3A%2F%2Fdev.mysql.com%2Fdoc%2Frefman%2F5.5%2Fen%2Fset.html&amp;token=ad8b5db0c68a8b20fa3ec7622563e0cd" target="mysql_doc"><span class="syntax_alpha syntax_alpha_reservedWord">SET</span></a> <span class="syntax_alpha syntax_alpha_identifier">time_zone</span> <span class="syntax_punct">=</span>  <span class="syntax_quote syntax_quote_double">&quot;+00:00&quot;</span></span></span>
+    </p>
+<p>
+    <strong>MySQL a répondu: </strong><a href="./url.php?url=http%3A%2F%2Fdev.mysql.com%2Fdoc%2Frefman%2F5.5%2Fen%2Ferror-messages-server.html&amp;token=ad8b5db0c68a8b20fa3ec7622563e0cd" target="mysql_doc"><img class="icon" src="./themes/pmahomme/img/b_help.png" width="11" height="11" alt="Documentation" title="Documentation" /></a>
+</p>
+<code>
+#1298 - Unknown or incorrect time zone: '+00:00'
+</code><br />
+</div><script type="text/javascript">
+//<![CDATA[
+$(document).ready(function(){
+// updates current settings
+if (window.parent.setAll) {
+    window.parent.setAll('fr', 'utf32_general_ci', '1', 'ecommerce', '', 'ad8b5db0c68a8b20fa3ec7622563e0cd');
+}
+    // set current db, table and sql query in the querywindow
+if (window.parent.reload_querywindow) {
+    window.parent.reload_querywindow(
+        'ecommerce',
+        '',
+        '');
+}
+    
+if (window.parent.frame_content) {
+    // reset content frame name, as querywindow needs to set a unique name
+    // before submitting form data, and navigation frame needs the original name
+    if (typeof(window.parent.frame_content.name) != 'undefined'
+     && window.parent.frame_content.name != 'frame_content') {
+        window.parent.frame_content.name = 'frame_content';
+    }
+    if (typeof(window.parent.frame_content.id) != 'undefined'
+     && window.parent.frame_content.id != 'frame_content') {
+        window.parent.frame_content.id = 'frame_content';
+    }
+    //window.parent.frame_content.setAttribute('name', 'frame_content');
+    //window.parent.frame_content.setAttribute('id', 'frame_content');
+}
+});
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Base de données: `ecommerce`
---
-
--- --------------------------------------------------------
-
---
--- Structure de la table `articles`
---
-
-CREATE TABLE IF NOT EXISTS `articles` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `ref` varchar(20) NOT NULL,
-  `theme` varchar(20) NOT NULL,
-  `description` text NOT NULL,
-  `prix` float NOT NULL,
-  `image` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
-
---
--- Contenu de la table `articles`
---
-
-INSERT INTO `articles` (`id`, `ref`, `theme`, `description`, `prix`, `image`) VALUES
-(1, 'PAYSAGE1', '', 'Premier paysage', 1000.68, ''),
-(2, 'PAYSAGE2', '', 'Deuxième paysage', 500.49, ''),
-(3, 'PAYSAGE3', 'Liberté', 'La liberté est à conquérir sans cesse.', 5468.85, '');
-
--- --------------------------------------------------------
-
---
--- Structure de la table `membres`
---
-
-CREATE TABLE IF NOT EXISTS `membres` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `pseudo` varchar(20) NOT NULL,
-  `mail` varchar(200) NOT NULL,
-  `mdp` varchar(32) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
-
---
--- Contenu de la table `membres`
---
-
-INSERT INTO `membres` (`id`, `pseudo`, `mail`, `mdp`) VALUES
-(1, 'guguspass', '', 'test'),
-(2, 'aerich', 'ddd@ddd.com', 'bary0562'),
-(3, 'aerich2', 'ddd@ddd.com', 'barybary');
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+//]]>
+</script>
+</body>
+</html>
